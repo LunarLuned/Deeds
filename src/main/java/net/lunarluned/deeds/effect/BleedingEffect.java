743a9248a177ae4a7aad7f2484b242f9ea7f7131
LunarLuned <100322289+LunarLuned@users.noreply.github.com>
2023-01-12 22:47:28 -0800

    unique Bleeding Effect & en.us additions

--- src/main/java/net/lunarluned/deeds/effect/BleedingEffect.java
+++ src/main/java/net/lunarluned/deeds/effect/BleedingEffect.java
@@ -5,23 +5,37 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import org.jetbrains.annotations.NotNull;
 
 public class BleedingEffect extends MobEffect {
-    protected BleedingEffect(MobEffectCategory statusEffectCategory, int color) {
+    public BleedingEffect(MobEffectCategory statusEffectCategory, int color) {
         super(statusEffectCategory, color);
     }
 
     @Override
-    public void applyEffectTick(@NotNull LivingEntity pLivingEntity, int pAmplifier) {
-        if (pLivingEntity instanceof Player player && player.getRandom().nextInt(3) <= 3) {
-
-            player.hurt(new Deeds.DemonicDamageSource(), 1);
-            player.level.playSound(null, player.getOnPos(), SoundEvents.BOAT_PADDLE_WATER, SoundSource.PLAYERS, 1.0F, 1.0F);
+    public void applyEffectTick(LivingEntity entity, int amplifier) {
+        if (entity instanceof Player player && player.getRandom().nextInt(100) <= 5) {
+            if (player.getHealth() >= 4) {
+                player.level.playSound(null, player.getOnPos(), SoundEvents.BOAT_PADDLE_WATER, SoundSource.PLAYERS, 1.0F, 1.0F);
+                entity.hurt(new Deeds.DemonicDamageSource(), 1);
+                super.applyEffectTick(entity, amplifier);
+            }
+            if (player.getHealth() <= 5) {
+                if (player.getRandom().nextInt(100) <= 32 && !player.hasEffect(MobEffects.MOVEMENT_SLOWDOWN) || !player.hasEffect(ModEffects.STAGNATED)) {
+                    player.addEffect(new MobEffectInstance(ModEffects.STAGNATED, 120, 0));
+                } else if (player.getRandom().nextInt(100) >= 72 && !player.hasEffect(ModEffects.STAGNATED) || !player.hasEffect(MobEffects.MOVEMENT_SLOWDOWN) ) {
+                    player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 140, 0));
+                }
+            }
         }
-        super.applyEffectTick(pLivingEntity, pAmplifier);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int pDuration, int pAmplifier) {
+        return true;
     }
 
 }
